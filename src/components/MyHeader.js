43e82a75590663f27de922b6f37f611841aaeb7f
David Orson <onlyorson@outlook.com>
2020-04-16 00:57:07 +0100

    pre firebase commit

--- src/components/MyHeader.js
+++ src/components/MyHeader.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../Assets/images/surfsecrets_logo.png";
 
 function MyHeader() {
   return (
     <div className="header">
       <img className="logo" src={logo}></img>
-      <button className="button">SIGNUP</button>
+      <Link to="/Signup">
+        <button className="button">SIGNUP</button>
+      </Link>
       <button className="button">LOGIN</button>
     </div>
   );
