efe5371ffd59b5a1ea1c1c2f754ea1dd3cbb3517
David Orson <onlyorson@outlook.com>
2020-04-16 03:06:47 +0100

    just push...

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 
 import firebase, { fAuth, fDb } from "../config/fbConfig";
@@ -65,34 +64,6 @@ class Signup extends Component {
       </div>
     );
   }
-=======
-import React from "react";
-
-function Signup() {
-  return (
-    <div className="signup">
-      <h4>Sign up</h4>
-      <form className="signup-form" id="signup-form">
-        <label for="signup-email">Email Address</label>
-        <br />
-        <input className="input" type="email" id="signup-email" required />
-
-        <br />
-        <label for="signup-password">Password</label>
-        <br />
-        <input
-          className="input"
-          type="password"
-          id="signup-password"
-          required
-        />
-
-        <br />
-        <button className="button">SIGNUP</button>
-      </form>
-    </div>
-  );
->>>>>>> aaddb1f56e917162ad90680d6243328d6a82a644
 }
 
 export default Signup;
