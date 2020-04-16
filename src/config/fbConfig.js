21f450f089e32f16c8b9332ceb3566af8b81a735
David Orson <onlyorson@outlook.com>
2020-04-16 02:58:55 +0100

    signup success (may have errors)

---
+++ src/config/fbConfig.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyD1WqTClHQyNADcUhLy2rU8a0hseSty4pE",
+  authDomain: "cs-surf-secrets.firebaseapp.com",
+  databaseURL: "https://cs-surf-secrets.firebaseio.com",
+  projectId: "cs-surf-secrets",
+  storageBucket: "cs-surf-secrets.appspot.com",
+  messagingSenderId: "796192673519",
+  appId: "1:796192673519:web:795ba162f0e04562ded51a",
+  measurementId: "G-VGYBZNFS1W",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export const fAuth = firebase.auth();
+export const fDb = firebase.firestore();
+
+export default firebase;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,29 +1,69 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Signup() {
-  return (
-    <div className="signup">
-      <h4>Sign up</h4>
-      <form className="signup-form" id="signup-form">
-        <label for="signup-email">Email Address</label>
-        <br />
-        <input className="input" type="email" id="signup-email" required />
+import firebase, { fAuth, fDb } from "../config/fbConfig";
 
-        <br />
-        <label for="signup-password">Password</label>
-        <br />
-        <input
-          className="input"
-          type="password"
-          id="signup-password"
-          required
-        />
+class Signup extends Component {
+  constructor(props) {
+    super(props);
 
-        <br />
-        <button className="button">SIGNUP</button>
-      </form>
-    </div>
-  );
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleEmailChange = (event) => {
+    this.setState({
+      email: event.target.value,
+    });
+  };
+
+  handlePasswordChange = (event) => {
+    this.setState({
+      password: event.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`${this.state.email}${this.state.password}`);
+    const email = this.state.email;
+    const password = this.state.password;
+    fAuth.createUserWithEmailAndPassword(email, password).then((cred) => {
+      // reset form
+    });
+  };
+
+  render() {
+    return (
+      <div className="signup">
+        <h4>Sign up</h4>
+        <form className="signup-form" onSubmit={this.handleSubmit}>
+          <div>
+            <label>Email Address</label>
+            <input
+              className="input"
+              type="email"
+              value={this.state.email}
+              onChange={this.handleEmailChange}
+            />
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              className="input"
+              type="password"
+              value={this.state.password}
+              onChange={this.handlePasswordChange}
+            />
+          </div>
+          <button className="button" type="submit">
+            SIGNUP
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Signup;
