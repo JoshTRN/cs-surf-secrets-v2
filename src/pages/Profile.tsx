cbea76baa8a6332b84fba2fab665ab50910c096f
David Orson <onlyorson@outlook.com>
2020-04-19 22:47:52 +0100

    Email Displayed on Profile Page when LoggedIn

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
+import { fAuth } from "../config/fbConfig";
 
-function ProfilePage() {
-  return (
-    <div>
-      <h1>Proffffiiillleee Pageeeee</h1>
-    </div>
-  );
+class ProfilePage extends Component<any, any> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      loggedIn: false,
+    };
+  }
+
+  authEvent = fAuth.onAuthStateChanged((user) => {
+    this.setState({
+      loggedIn: user ? true : false,
+    });
+    this.setState({
+      email: user?.email,
+    });
+  });
+
+  render() {
+    if (this.state.loggedIn) {
+      return (
+        <div>
+          {/* Conditional rendering when logged in/not */}
+          <h1>{this.state.email}</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          {/* Conditional rendering when logged in/not */}
+          <h1>Login to see profile</h1>
+        </div>
+      );
+    }
+  }
 }
-
 export default ProfilePage;
