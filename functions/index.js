ab1f46542fbad9c75f1a6a540a5f065fe78a85cb
David Orson <onlyorson@outlook.com>
2020-05-01 05:06:07 +0100

    Cloud Functions - Auth

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,27 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const app = require("express")();
 
 admin.initializeApp();
 
-const express = require("express");
-const app = express();
+const firebaseConfig = {
+  apiKey: "AIzaSyD1WqTClHQyNADcUhLy2rU8a0hseSty4pE",
+  authDomain: "cs-surf-secrets.firebaseapp.com",
+  databaseURL: "https://cs-surf-secrets.firebaseio.com",
+  projectId: "cs-surf-secrets",
+  storageBucket: "cs-surf-secrets.appspot.com",
+  messagingSenderId: "796192673519",
+  appId: "1:796192673519:web:795ba162f0e04562ded51a",
+  measurementId: "G-VGYBZNFS1W",
+};
+
+const firebase = require("firebase");
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
 
 app.get("/posts", (req, res) => {
-  admin
-    .firestore()
-    .collection("posts")
+  db.collection("posts")
     .orderBy("createdAt", "desc")
     .get()
     .then((data) => {
@@ -37,9 +49,7 @@ app.post("/post", (req, res) => {
     createdAt: new Date().toISOString(),
   };
 
-  admin
-    .firestore()
-    .collection("posts")
+  db.collection("posts")
     .add(newPost)
     .then((doc) => {
       res.json({ message: `document ${doc.id} created successfully` });
@@ -50,4 +60,54 @@ app.post("/post", (req, res) => {
     });
 });
 
-exports.api = functions.https.onRequest(app);
+// Signup
+
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+
+  // Validation
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: `this handle is already taken` });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email is already in use " });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+});
+
+exports.api = functions.region("europe-west1").https.onRequest(app);
