4fc50a16209ed824e6ef31f00d8985cdc4f6f68b
David Orson <onlyorson@outlook.com>
2020-05-05 10:45:53 +0100

    Token Decoding with jwt-decode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Router, RouteComponentProps } from "@reach/router";
+import jwtDecode from "jwt-decode";
 
 import HomePage from "./pages/Home";
 import SignupPage from "./pages/Signup";
@@ -14,6 +15,19 @@ const Home = (props: RouteComponentProps) => <HomePage />;
 const Profile = (props: RouteComponentProps) => <ProfilePage />;
 const Signup = (props: RouteComponentProps) => <SignupPage />;
 const Login = (props: RouteComponentProps) => <LoginPage />;
+
+// conditional Routing TODO
+let authenticated;
+const token = localStorage.FBIdToken;
+if (token) {
+  const decodedToken: any = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = "/login";
+    authenticated = false;
+  } else {
+    authenticated = true;
+  }
+}
 
 function App() {
   return (
